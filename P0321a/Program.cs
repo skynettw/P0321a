fea4ba563e1dca0f7fd8377f266a041eb8dc4325
skynettw <skynet.tw@gmail.com>
2023-03-21 11:59:18 +0800

    final update

--- P0321a/Program.cs
+++ P0321a/Program.cs
@@ -96,9 +96,23 @@ namespace P0321a
             
 
         }
+
+        static void Practice5()
+        {
+            // 九九乘法表
+            for (int i=1; i<=9; i++)
+            {
+                for (int j=1; j<=9; j++)
+                {
+                    int res = i * j;
+                    Console.WriteLine($"{i}*{j}={res}");
+                }
+                Console.WriteLine();
+            }
+        }
         static void Main(string[] args)
         {
-            Practice4();
+            Practice5();
             Console.ReadLine();
         }
     }
