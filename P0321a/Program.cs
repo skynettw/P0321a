2cfd513d6bc705c7b8ad2fb4260e8f4a2fb5ece6
skynettw <skynet.tw@gmail.com>
2023-03-21 11:23:43 +0800

    move the codes into their methods

--- P0321a/Program.cs
+++ P0321a/Program.cs
@@ -8,11 +8,9 @@ namespace P0321a
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Practice1()
         {
-
             // 讓使用者輸入 n，然後計算1+2+...+n的總和是多少？
-            /*
             int n;         //使用者輸入的 n
             int total = 0; // 要用來儲存總和的變數
             Console.Write("n=");
@@ -24,7 +22,10 @@ namespace P0321a
             }
 
             Console.WriteLine($"1+2+...+{n}={total}");
-            
+        }
+
+        static void Practice2()
+        {
             // 讓使用者輸入 n，然後計算1*2*...*n的階乘值是多少？
             int n;         //使用者輸入的 n
             long total = 1; // 要用來儲存階乘的變數
@@ -37,16 +38,20 @@ namespace P0321a
             }
 
             Console.WriteLine($"{n}!={total}");
-            */
+        }
+
+        static void Practice3()
+        {
             // 讓使用者輸入 n (n>1)，判斷n是否為質數 
 
             int n;         //使用者輸入的 n
             Console.Write("n=");
             n = Convert.ToInt32(Console.ReadLine());
             bool isPrime = true;
-            for(int i=2; i<=n/2; i++)
+            for (int i = 2; i <= n / 2; i++)
             {
-                if (n%i==0) {
+                if (n % i == 0)
+                {
                     isPrime = false;
                     break;
                 }
@@ -54,11 +59,16 @@ namespace P0321a
             if (isPrime)
             {
                 Console.WriteLine($"{n}是質數！");
-            } else
+            }
+            else
             {
                 Console.WriteLine($"{n}不是質數！");
             }
 
+        }
+        static void Main(string[] args)
+        {
+            Practice3();
             Console.ReadLine();
         }
     }
