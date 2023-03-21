9d9fbbc4ecb84ed47f5692089fab764beecfa5b6
skynettw <skynet.tw@gmail.com>
2023-03-21 10:52:32 +0800

    add prime number check

--- P0321a/Program.cs
+++ P0321a/Program.cs
@@ -24,7 +24,7 @@ namespace P0321a
             }
 
             Console.WriteLine($"1+2+...+{n}={total}");
-            */
+            
             // 讓使用者輸入 n，然後計算1*2*...*n的階乘值是多少？
             int n;         //使用者輸入的 n
             long total = 1; // 要用來儲存階乘的變數
@@ -37,6 +37,28 @@ namespace P0321a
             }
 
             Console.WriteLine($"{n}!={total}");
+            */
+            // 讓使用者輸入 n (n>1)，判斷n是否為質數 
+
+            int n;         //使用者輸入的 n
+            Console.Write("n=");
+            n = Convert.ToInt32(Console.ReadLine());
+            bool isPrime = true;
+            for(int i=2; i<n/2; i++)
+            {
+                if (n%i==0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime)
+            {
+                Console.WriteLine($"{n}是質數！");
+            } else
+            {
+                Console.WriteLine($"{n}不是質數！");
+            }
+
             Console.ReadLine();
         }
     }
