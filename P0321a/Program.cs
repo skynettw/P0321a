7e47eff224038761f158f00538e6d619e65ab8e7
skynettw <skynet.tw@gmail.com>
2023-03-21 10:53:33 +0800

    fix an error

--- P0321a/Program.cs
+++ P0321a/Program.cs
@@ -44,7 +44,7 @@ namespace P0321a
             Console.Write("n=");
             n = Convert.ToInt32(Console.ReadLine());
             bool isPrime = true;
-            for(int i=2; i<n/2; i++)
+            for(int i=2; i<=n/2; i++)
             {
                 if (n%i==0) {
                     isPrime = false;
