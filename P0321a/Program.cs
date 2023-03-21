ae6307d9ad695f8ad691d286e80de048df11cd93
skynettw <skynet.tw@gmail.com>
2023-03-21 10:35:03 +0800

    加上階乘的程式碼

--- P0321a/Program.cs
+++ P0321a/Program.cs
@@ -12,6 +12,7 @@ namespace P0321a
         {
 
             // 讓使用者輸入 n，然後計算1+2+...+n的總和是多少？
+            /*
             int n;         //使用者輸入的 n
             int total = 0; // 要用來儲存總和的變數
             Console.Write("n=");
@@ -23,6 +24,19 @@ namespace P0321a
             }
 
             Console.WriteLine($"1+2+...+{n}={total}");
+            */
+            // 讓使用者輸入 n，然後計算1*2*...*n的階乘值是多少？
+            int n;         //使用者輸入的 n
+            long total = 1; // 要用來儲存階乘的變數
+            Console.Write("n=");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                total *= i;
+            }
+
+            Console.WriteLine($"{n}!={total}");
             Console.ReadLine();
         }
     }
